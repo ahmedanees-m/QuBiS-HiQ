da228b1987fbc7b33b8fc12d326ee1849864969a
Anees Ahmed Mahaboob Ali <ahmedaneesm@gmail.com>
2026-03-30 02:40:31 +0530

    Add qubis_hiq/__init__.py to make package importable

---
+++ qubis_hiq/__init__.py
@@ -0,0 +1,19 @@
+"""QuBiS-HiQ: physics-informed quantum circuit for DNA thermodynamics."""
+from .santalucia import compute_total_dg, get_nn_dg, boltzmann_sigmoid
+from .encoding import apply_encoding_layer, encode_nucleotide
+from .circuit_builder import build_circuit, build_ablation_variants
+from .feature_extraction import extract_feature_vector, feature_vector_dim
+from .vienna_interface import predict_structure
+
+__all__ = [
+    "compute_total_dg",
+        "get_nn_dg",
+            "boltzmann_sigmoid",
+                "apply_encoding_layer",
+                    "encode_nucleotide",
+                        "build_circuit",
+                            "build_ablation_variants",
+                                "extract_feature_vector",
+                                    "feature_vector_dim",
+                                        "predict_structure",
+                                        ]
